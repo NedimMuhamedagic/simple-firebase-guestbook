33ff675c1c8a60aa3abefc6fb7ecf0e7024be926
Nedim Muhamedagic <muhamedagic.n@gmail.com>
2018-04-19 01:26:36 +0200

    fix: flow errors, remove tests, eslint rules

--- src/App.js
+++ src/App.js
@@ -6,11 +6,14 @@ import './App.css';
 type AppProps = {};
 
 class App extends Component<AppProps> {
-  render() {
+  render(): ?React$Element<any> {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img
+            alt="logo"
+            className="App-logo"
+            src={ logo } />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
