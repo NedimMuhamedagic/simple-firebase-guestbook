b7bf3a561c7f620666ac7eb12e77bc65aa8aa9ea
Nedim Muhamedagic <muhamedagic.n@gmail.com>
2018-04-18 23:30:01 +0200

    add flow

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+// @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const addNumbers = (numbers: Array<number>): number => {
+  return 2;
+}
+
+addNumbers(1, 2, '3');
+
+ReactDOM.render(
+  <App />,
+  (document.getElementById('root'): any)
+  // TODO: [@NedimMuhamedagic]:
+  // Add proper checkup to make sure an element
+  // with id 'root' exists
+);
 registerServiceWorker();
