06db4ceac3674eaf2032e9a5ba63945d7e053180
Nedim Muhamedagic <muhamedagic.n@gmail.com>
2018-04-19 19:14:38 +0200

    chore: add sass and fix babel build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './styles/manifest.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
